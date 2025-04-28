74cd4ed52593e1094492a6001cc923eb95fd07a7
veroanggraa <anggrainiveronica227@gmail.com>
2025-04-28 16:44:41 +0700

    add barcode usecase

---
+++ app/src/main/java/com/veroanggra/barcodescannerapplication/domain/ScanBarcodeUseCase.kt
@@ -0,0 +1,27 @@
+package com.veroanggra.barcodescannerapplication.domain
+
+import android.content.Context
+import androidx.camera.mlkit.vision.MlKitAnalyzer
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.veroanggra.barcodescannerapplication.repositories.BarcodeRepository
+import kotlinx.coroutines.flow.Flow
+
+class ScanBarcodeUseCase(private val barcodeRepository: BarcodeRepository) {
+    fun execute(
+        context: Context,
+        targetCoordinateSystem: Int,
+        onResult: (List<Barcode>?) -> Unit
+    ): MlKitAnalyzer {
+        val barcodeScanner: BarcodeScanner = barcodeRepository.getBarcodeScannerInstance()
+        return MlKitAnalyzer(
+            listOf(barcodeScanner),
+            targetCoordinateSystem,
+            ContextCompat.getMainExecutor(context)
+        ) { resultValue ->
+            val barcodes: List<Barcode>? = resultValue.getValue(barcodeScanner)
+            onResult(barcodes)
+        }
+    }
+}
